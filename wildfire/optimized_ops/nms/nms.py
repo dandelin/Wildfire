119be916667aa183097355cc21f9cc55507d4a66
dandelin.kim <dandelin.kim@kakaocorp.com>
2018-10-04 14:14:32 +0900

    - WIDER raw format dataset - Soft NMS on wildfire - SGDR with cosine annealing

--- wildfire/optimized_ops/nms/nms.py
+++ wildfire/optimized_ops/nms/nms.py
@@ -1,5 +1,7 @@
 from numba import jit
 import numpy as np
+import pdb
+import torch
 
 @jit
 def nms_kernel(boxes, thresh):
@@ -37,7 +39,102 @@ def nms_kernel(boxes, thresh):
 
     return np.where(suppressed == 0)[0]
 
-def nms(boxes, criteria, thresh, pre=None, post=None):
+@jit
+def soft_nms_kernel(boxes, sigma=0.5, Nt=0.3, threshold=0.001, method=0):
+    N = boxes.shape[0]
+
+    inds = np.arange(N)
+
+    for i in range(N):
+        maxscore = boxes[i, 4]
+        maxpos = i
+
+        tx1 = boxes[i, 0]
+        ty1 = boxes[i, 1]
+        tx2 = boxes[i, 2]
+        ty2 = boxes[i, 3]
+        ts = boxes[i, 4]
+        ti = inds[i]
+
+        pos = i + 1
+        # get max box
+        while pos < N:
+            if maxscore < boxes[pos, 4]:
+                maxscore = boxes[pos, 4]
+                maxpos = pos
+            pos = pos + 1
+
+        # add max box as a detection
+        boxes[i, 0] = boxes[maxpos, 0]
+        boxes[i, 1] = boxes[maxpos, 1]
+        boxes[i, 2] = boxes[maxpos, 2]
+        boxes[i, 3] = boxes[maxpos, 3]
+        boxes[i, 4] = boxes[maxpos, 4]
+        inds[i] = inds[maxpos]
+
+        # swap ith box with position of max box
+        boxes[maxpos, 0] = tx1
+        boxes[maxpos, 1] = ty1
+        boxes[maxpos, 2] = tx2
+        boxes[maxpos, 3] = ty2
+        boxes[maxpos, 4] = ts
+        inds[maxpos] = ti
+
+        tx1 = boxes[i, 0]
+        ty1 = boxes[i, 1]
+        tx2 = boxes[i, 2]
+        ty2 = boxes[i, 3]
+
+        pos = i + 1
+        # NMS iterations, note that N changes if detection boxes fall below
+        # threshold
+        while pos < N:
+            x1 = boxes[pos, 0]
+            y1 = boxes[pos, 1]
+            x2 = boxes[pos, 2]
+            y2 = boxes[pos, 3]
+
+            area = (x2 - x1 + 1) * (y2 - y1 + 1)
+            iw = (min(tx2, x2) - max(tx1, x1) + 1)
+            if iw > 0:
+                ih = (min(ty2, y2) - max(ty1, y1) + 1)
+                if ih > 0:
+                    ua = float((tx2 - tx1 + 1) * (ty2 - ty1 + 1) + area - iw * ih)
+                    ov = iw * ih / ua  # iou between max box and detection box
+
+                    if method == 1:  # linear
+                        if ov > Nt:
+                            weight = 1 - ov
+                        else:
+                            weight = 1
+                    elif method == 2:  # gaussian
+                        weight = np.exp(-(ov * ov) / sigma)
+                    else:  # original NMS
+                        if ov > Nt:
+                            weight = 0
+                        else:
+                            weight = 1
+
+                    boxes[pos, 4] = weight * boxes[pos, 4]
+
+                    # if box score falls below threshold, discard the box by
+                    # swapping with last box update N
+                    if boxes[pos, 4] < threshold:
+                        boxes[pos, 0] = boxes[N - 1, 0]
+                        boxes[pos, 1] = boxes[N - 1, 1]
+                        boxes[pos, 2] = boxes[N - 1, 2]
+                        boxes[pos, 3] = boxes[N - 1, 3]
+                        boxes[pos, 4] = boxes[N - 1, 4]
+                        inds[pos] = inds[N - 1]
+                        N = N - 1
+                        pos = pos - 1
+
+            pos = pos + 1
+
+    return boxes[:N], inds[:N]
+
+
+def nms(boxes, criteria, thresh, pre=None, post=None, soft=False):
     """
     :param boxes: Tensor [N, 4], unsorted
     :param criteria: Tensor [N]
@@ -49,7 +146,12 @@ def nms(boxes, criteria, thresh, pre=None, post=None):
     crits, sort_idx = crits[:pre], sort_idx[:pre]
     bbox = boxes[sort_idx]
 
-    keep = nms_kernel(bbox.cpu().numpy(), thresh)
+    if soft:
+        boxes = torch.cat([bbox, crits.unsqueeze(1)], dim=1)
+        _, keep = soft_nms_kernel(boxes.cpu().numpy())
+    else:
+        keep = nms_kernel(bbox.cpu().numpy(), thresh)
+
     keep = keep[:post]
 
     return sort_idx[keep]
